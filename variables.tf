b1b2f3b8132447d119e635ffb846323c5a84bdfb
189preetis <18preetigzp@gmail.com>
2026-01-02 10:53:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "ec2_instance_type" {
       default = "t2.micro"
-      type="string"
+      type=string
 }
 
 
 variable "ec2_ami_id" {
     default = "ami-02b8269d5e85954ef"
-    type="string"
+    type=string
 }
